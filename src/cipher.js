0ada70000a64aedcc93e320ed1f7112652cb7f9a
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-02-23 22:51:00 -0500

    Cambio en la funcion para encriptar

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,45 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+  /* metodo de cifrado */
+encode: (offset, string) => {
+
+let  encriptedWord ='';
+
+for (let i =0; i<string.length ; i++){
+
+let numAscci = string.charCodeAt (i);
+    if (numAscci>=65 && numAscci<90){
+
+       encriptedWord += String.fromCharCode(((numAscci) - 65 + parseInt(offset))%26 + 65) ;
+    }
+
+
+}
+
+return encriptedWord;
+
+},
+
+
+
+decode:(offset,string)=> {
+
+let  desencriptedWord ='';
+
+for (let i =0; i<string.length ; i++){
+
+let numAscci = string.charCodeAt (i);
+    if (numAscci>=65 && numAscci<90){
+
+  desencriptedWord += String.fromCharCode ((numAscci - 90 - parseInt(offset) )%26 + 90);
+    }
+
+
+}
+
+return desencriptedWord;
+
+
+
+}
+
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 
 
-function showName(){
+document.getElementById("encriptForm").style.display="none";
+
+const showName= ()=>{
     let username= document.getElementById('user').value;
      document.getElementById("named").innerHTML= ("Bienvenid@  "+ username);
-    
+     document .getElementById("encriptForm").style.display ="initial";
+};
 
- }
+
+
+const showText = () => {
+
+
+
+     let getText =document.getElementById ("firstText").value;
+     let encriptNumber =document.getElementById ("getASCCI").value;
+     document.getElementById("textoAqui").innerHTML = (window.cipher.encode(encriptNumber,getText));
+
+
+};
