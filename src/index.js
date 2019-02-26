2d348e426da27204d9c7cc4d8c6b0c036aead30a
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-02-26 00:18:26 -0500

    cambios de encriptado

--- src/index.js
+++ src/index.js
@@ -12,22 +12,31 @@ const showName= ()=>{
 
 const showText = () => {
 
-
-
      let getText =document.getElementById ("firstText").value;
      let encriptNumber =document.getElementById ("getASCCI").value;
      document.getElementById("textoAqui").innerHTML = (window.cipher.encode(encriptNumber,getText));
-
-
 };
 
 const showTextDesencript = () => {
-
-
 
      let getText =document.getElementById ("firstText").value;
      let encriptNumber =document.getElementById ("getASCCI").value;
      document.getElementById("textoAqui").innerHTML = (window.cipher.decode(encriptNumber,getText));
 
+};
 
+const cleaner = () =>  {
+     let getText =document.getElementById ("firstText").value ="  ";
+     let encriptNumber =document.getElementById ("getASCCI").value ="   ";
+     
+     document.getElementById("clean").innerHTML = (getText,encriptNumber);
+
+
+
+};
+
+const back =() =>  {
+
+     
+   location.reload();
 };
