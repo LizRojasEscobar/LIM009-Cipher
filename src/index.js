859c2c3e18278daf94b23442ba7ac17aa41e7542
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-03-05 01:14:22 -0500

    cambios sobre css - medidas

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 
 //Se esconde el formulario
-document.getElementById("encriptForm").style.display="none";
+document.getElementById('encriptForm').style.display='none';
 
 //Funcion para obtener el nombre
 const showName= ()=>{
     let username= document.getElementById('user').value;
-     document.getElementById("named").innerHTML= ("Bienvenid@  "+ username);
-     document .getElementById("encriptForm").style.display ="initial";
+     document.getElementById('named').innerHTML= ('Bienvenid@  '+ username);
+     document .getElementById('encriptForm').style.display ='initial';
 }; 
 
 //Eventos de los botones 
-const eventofButtons = document.getElementById("bd");
-eventofButtons.addEventListener("click", (e)=>{
+const eventofButtons = document.getElementById('bd');
+eventofButtons.addEventListener('click', (e)=>{
 
-     let getText =document.getElementById ("firstText").value;
-     let encriptNumber =document.getElementById ("getASCCI").value;
+     let getText =document.getElementById ('firstText').value;
+     let encriptNumber =document.getElementById ('getASCCI').value;
 
 switch (e.target.id){
  
-     case "boton1":
+     case 'boton1':
           showName();
      break;
-     case "encriptButton":
-     document.getElementById("textoAqui").innerHTML = (window.cipher.encode(encriptNumber,getText));
+     case 'encriptButton':
+     document.getElementById('textoAqui').innerHTML = (window.cipher.encode(encriptNumber,getText));
      break;
 
-     case "desencriptButton":
+     case 'desencriptButton':
      
-     document.getElementById("textoAqui").innerHTML = (window.cipher.decode(encriptNumber,getText));
+     document.getElementById('textoAqui').innerHTML = (window.cipher.decode(encriptNumber,getText));
      
      break;
 
-     case "clean":
+     case 'clean':
 
-     document.getElementById ("firstText").value ="  ";
-     document.getElementById ("getASCCI").value ="   ";
+     document.getElementById ('firstText').value =' ';
+     document.getElementById ('getASCCI').value =' ';
      
      
      break;
       
-     case "back":
+     case 'back':
      location.reload();
 
      break;
