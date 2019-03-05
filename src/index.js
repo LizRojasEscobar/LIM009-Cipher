e7512b45b200772a5ea23fff3ba76222997a5198
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-03-04 20:47:28 -0500

    correccion del codigo de las funciones

--- src/index.js
+++ src/index.js
@@ -1,47 +1,15 @@
 
-
+//Se esconde el formulario
 document.getElementById("encriptForm").style.display="none";
 
+//Funcion para obtener el nombre
 const showName= ()=>{
     let username= document.getElementById('user').value;
      document.getElementById("named").innerHTML= ("Bienvenid@  "+ username);
      document .getElementById("encriptForm").style.display ="initial";
-};
+}; 
 
-/*
-
-const showText = () => {
-
-     let getText =document.getElementById ("firstText").value;
-     let encriptNumber =document.getElementById ("getASCCI").value;
-     document.getElementById("textoAqui").innerHTML = (window.cipher.encode(encriptNumber,getText));
-};
-
-const showTextDesencript = () => {
-
-     let getText =document.getElementById ("firstText").value;
-     let encriptNumber =document.getElementById ("getASCCI").value;
-     document.getElementById("textoAqui").innerHTML = (window.cipher.decode(encriptNumber,getText));
-
-};
-
-const cleaner = () =>  {
-     let getText =document.getElementById ("firstText").value ="  ";
-     let encriptNumber =document.getElementById ("getASCCI").value ="   ";
-     
-     document.getElementById("clean").innerHTML = (getText,encriptNumber);
-
-
-
-};
-
-const back =() =>  {
-
-     
-   location.reload();
-};
-
-*/
+//Eventos de los botones 
 const eventofButtons = document.getElementById("bd");
 eventofButtons.addEventListener("click", (e)=>{
 
@@ -76,11 +44,6 @@ switch (e.target.id){
 
      break;
 
-
-
-
 }
-
-
 
 });
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,31 +1,59 @@
 describe('cipher', () => {
 
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
-
-  describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
+    it('debería ser un objeto', () => {
+      assert.equal(typeof cipher, 'object');
     });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33',()=>{
-      assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
-    });
-  });
-
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
-
   
-  it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',()=>{
+    describe('cipher.encode', () => {
+  
+      it('debería ser una función', () => {
+        assert.equal(typeof cipher.encode, 'function');
+      });
+  
+      it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33',()=>{
+        assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+      });
+      
+      it('debería retornar "fghij" para "abcde" con offset 5',()=>{
+        assert.equal(cipher.encode(5,"abcde"),"fghij");
+      });
+      it('debería retornar "67890" para "12345" con offset 15',()=>{
+        assert.equal(cipher.encode(15,"12345"),"67890");
+      });
+      it('debería retornar "$ipmb" para "#hola" con offset 1',()=>{
+        assert.equal(cipher.encode(1,"#hola"),"$ipmb");
+      });
+      it('debería retornar "KRO D" para "HOL A" con offset 3',()=>{
+        assert.equal(cipher.encode(3,"HOL A"),"KRO D");
+      });
+      
+    });
+  
+    describe('cipher.decode', () => {
+  
+      it('debería ser una función', () => {
+        assert.equal(typeof cipher.decode, 'function');
+      });
+  
+    
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',()=>{
+  
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
 
-    assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+      
+    it('debería retornar "abcde" para "fghij" con offset 5',()=>{
+      assert.equal(cipher.decode(5,"fghij"),"abcde");
+    });
+
+    it('debería retornar "12345" para "67890" con offset 15',()=>{
+      assert.equal(cipher.decode(15,"67890"),"12345");
+    });
+    it('debería retornar "#hola" para "$ipmb" con offset 1',()=>{
+      assert.equal(cipher.decode(1,"$ipmb"),"#hola");
+    });
+    it('debería retornar "HOL A" para "KRO D" con offset 3',()=>{
+      assert.equal(cipher.decode(3,"KRO D"),"HOL A");
+    });
   });
-
-});
-});
+  });
